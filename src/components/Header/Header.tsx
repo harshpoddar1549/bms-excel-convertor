e71fbc0efaaf77f50412d20a61ddada492ccb950
PODDAR Harsh <I0001431@as.airbus.corp>
2023-09-27 18:10:05 +0530

    added BMS-to-Excel Converter on the top

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,8 @@ import React from 'react'
 function Header() {
   return (
 		<AppBar>
-			<Toolbar>
+			<Toolbar style={{justifyContent:"space-between"}}>
+				<Typography>BMS-to-Excel-Converter</Typography>
 				<Typography>Lahe Lahe</Typography>
 			</Toolbar>
 		</AppBar>
